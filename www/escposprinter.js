9002b3713fd9d315c67fb929b4eee327c85e2f34
Thanakrit Ratsami <james.lifestudio@gmail.com>
2018-05-04 14:46:40 +0700

    rename file

--- www/escposprinter.js
+++ www/escposprinter.js
@@ -1,7 +1,7 @@
 var exec = require('cordova/exec');
 
 exports.printAndroid = function (success, error, dataPrint, ipAddress, isShowLogo) {
-    exec(success, error, "escposipprinter", "printAndroid", [{
+    exec(success, error, "escposprinter", "printAndroid", [{
         "dataPrint": dataPrint,
         "ipAddress": ipAddress,
         "isShowLogo": isShowLogo
